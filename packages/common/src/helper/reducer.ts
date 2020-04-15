baaea6af13aee3853224b50fa15828ecbb028d37
mro <254225961@qq.com>
2020-04-15 12:33:50 +0800

    fix(@dxjs/common): getMetadata 参数错误

--- packages/common/src/helper/reducer.ts
+++ packages/common/src/helper/reducer.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { REDUCER_METHODS_KEY } from '@dxjs/shared/symbol';
+import { REDUCER_METHODS_KEY, SymbolType } from '@dxjs/shared/symbol';
 
-export function Reducer(type: string | symbol): MethodDecorator {
+export function Reducer(type?: string | symbol): MethodDecorator {
   return (
     target: any,
     key: string | symbol,
     descriptor: TypedPropertyDescriptor<any>,
   ): TypedPropertyDescriptor<any> => {
-    const reducers: Map<symbol | string, symbol | string> =
-      // 通过 action type 获取对应的 target 方法，所以用 Map 结构
-      Reflect.getMetadata(target, REDUCER_METHODS_KEY) ?? new Map<symbol | string, symbol>();
+    // 通过 action type 获取对应的 target 方法，所以用 Map 结构
+    const reducers: Map<SymbolType, SymbolType> =
+      Reflect.getMetadata(REDUCER_METHODS_KEY, target) ?? new Map();
 
     reducers.set(type || Symbol('__action'), key);
     Reflect.defineMetadata(REDUCER_METHODS_KEY, [], target);
