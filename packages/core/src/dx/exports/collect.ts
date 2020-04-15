59426056467763b6c0b1b12d4185315c1a082878
mro <254225961@qq.com>
2020-04-15 19:02:24 +0800

    feat(@dxjs/core): 在开发环境判断收集器手机的是否为空

--- packages/core/src/dx/exports/collect.ts
+++ packages/core/src/dx/exports/collect.ts
@@ -2,10 +2,23 @@
 import { DxModelContstructor } from '@dxjs/shared/interfaces/dx-model.interface';
 import { MODEL_NAME } from '@dxjs/shared/symbol';
 import { store } from '../../helper/store';
+import { DxModel } from '../../dx-model/model';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const invariant = require('invariant');
 
 export function collectFactory(inst: symbol) {
-  return (name: string) => {
+  return (name?: string) => {
     return function Decorate(ModelTarget: DxModelContstructor): DxModelContstructor {
+      if (process.env.NODE_ENV === 'development') {
+        invariant(
+          ModelTarget instanceof DxModel,
+          'collect model 必须继承自 DxModel, 当前 model 类型为 %s',
+          typeof ModelTarget,
+        );
+      }
+
       const models = store.getModels(inst);
       const _name = name || ModelTarget.name;
       Reflect.defineMetadata(MODEL_NAME, _name, ModelTarget);
