63140fc6dba04610ec929b6e83e92ab533429c88
mro <254225961@qq.com>
2020-04-22 18:34:11 +0800

    feat(@dxjs/core): 记录所有的 action type

--- packages/core/src/helper/store.ts
+++ packages/core/src/helper/store.ts
@@ -1,16 +1,26 @@
 import { DxEnhancer } from '@dxjs/shared/interfaces/dx-enhancer.interface';
 import { DxModelContstructor } from '@dxjs/shared/interfaces/dx-model.interface';
 import { Store } from 'redux';
+import { SymbolType } from '@dxjs/shared/symbol';
 
 interface ModelRefs {
   set: Set<DxModelContstructor>;
   map: { [key: string]: DxModelContstructor };
 }
 
+interface StoreActionType {
+  // 用于记录生成的 action type
+  reducers: Set<SymbolType>;
+  effects: Set<SymbolType>;
+}
+
 export const store = {
   enhancer: new Map<symbol, DxEnhancer>(),
   models: new Map<symbol, ModelRefs>(),
   reduxStore: new Map<symbol, Store>(),
+
+  // 用来记录 action, 以方便判断 action 是否 effect
+  actionTypes: new Map<symbol, StoreActionType>(),
 
   getModels(inst: symbol): ModelRefs {
     let models = store.models.get(inst);
