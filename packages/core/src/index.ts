a0ba762f1349f80900a9ab5b51328e037a6fb174
mro <254225961@qq.com>
2020-04-22 18:56:07 +0800

    style: 格式化代码

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
-import { DxFactory, IDxFactory } from './dx';
+import { DxFactory, DxFactoryInterface } from './dx';
 
 export { connect } from 'react-redux';
 export { DxModel } from './dx-model/model';
 export { DxFactory };
-export const Dx: IDxFactory = DxFactory();
+export const Dx: DxFactoryInterface = DxFactory();
