270bf0cabea3e229ffc0e0aed0307e71d4677861
mro <254225961@qq.com>
2020-04-22 18:38:56 +0800

    feat(@dxjs/core): 新增 isDxModel 工具函数

--- packages/core/src/dx-model/model.ts
+++ packages/core/src/dx-model/model.ts
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { DxBase } from './base';
-import { DxModelInterface } from '@dxjs/shared/interfaces/dx-model.interface';
+import { DxModelInterface, DxModelContstructor } from '@dxjs/shared/interfaces/dx-model.interface';
 
 export class DxModel extends DxBase implements DxModelInterface {
   state = {};
+}
+
+export function isDxModel(model: any): model is DxModelContstructor {
+  return model && model.prototype instanceof DxModel;
 }
