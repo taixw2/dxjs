d8419a4dec75d985692e26c8bc9198e0faddd146
mro <254225961@qq.com>
2020-04-28 15:45:48 +0800

    feat(@dxjs/core): 拆分 combin saga

--- packages/core/src/dx/create-effect/combin-saga.ts
+++ packages/core/src/dx/create-effect/combin-saga.ts
@@ -1,49 +1,13 @@
 import { EffectTypeInterface } from '@dxjs/shared/interfaces/dx-effect-type.interface';
 import { DxModelInterface } from '@dxjs/shared/interfaces/dx-model.interface';
-import { AnyAction } from 'redux';
-import { isGenerator } from './is-generator';
-import { TAKE_LATEST, TAKE_EVERY, TAKE_LEADING, THROTTLE } from '@dxjs/shared/symbol';
-import { spawn, takeLatest, takeEvery, takeLeading, throttle, ForkEffect } from 'redux-saga/effects';
-const invariant = require('invariant');
+import { ForkEffect } from 'redux-saga/effects';
+import { createEffectHandler } from './create-effect-handler';
+import { createEffectFork } from './create-effect-fork';
 
 export function combinSaga(model: DxModelInterface, meta: EffectTypeInterface): ForkEffect {
-  function* effectHandler(action: AnyAction): Generator {
-    try {
-      const currentEffect = Reflect.get(model, meta.name);
-      if (__DEV__) {
-        invariant(
-          currentEffect && isGenerator(currentEffect),
-          '副作用函数不是一个 generator 函数，函数名为: %s, 如果确定不是此原因，请提交 issus: %s',
-          meta.name,
-          '__ISSUE__',
-        );
-      }
-
-      const resolve = yield currentEffect.call(model, action.payload);
-      action.__dxjs_resolve?.(resolve);
-      return resolve;
-    } catch (error) {
-      action.__dxjs_reject?.(error);
-    }
+  function* effect(): Generator {
+    createEffectHandler(model, meta);
   }
 
-  return spawn(function*() {
-    switch (meta.helperType) {
-      case TAKE_EVERY:
-        yield takeEvery(meta.actionType, effectHandler);
-        break;
-      case TAKE_LATEST:
-        yield takeLatest(meta.actionType, effectHandler);
-        break;
-      case TAKE_LEADING:
-        yield takeLeading(meta.actionType, effectHandler);
-        break;
-      case THROTTLE:
-        yield throttle(meta.value[0] ?? 350, meta.actionType, effectHandler);
-        break;
-      default:
-        yield takeEvery(meta.actionType, effectHandler);
-        break;
-    }
-  });
+  return createEffectFork(meta, effect);
 }
