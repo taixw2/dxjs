a8ef37bae86aeb0c08afd346923f1de94b58a4f8
mro <254225961@qq.com>
2020-04-28 18:18:54 +0800

    refactor: create enhancer 移到 helper 中

--- packages/core/src/dx/create-store/store-enhancer.ts
+++ packages/core/src/dx/create-store/store-enhancer.ts
@@ -1,12 +1,12 @@
 import { CreateOption } from '@dxjs/shared/interfaces/dx-create-option.interface';
 import { store } from '../../helper/store';
-import { createEnhancer } from './create-enhancer';
+import { createEnhancer } from '../../helper/create-enhancer';
 
 export function storeEnhancer(inst: symbol, options: CreateOption): void {
   store.enhancer.set(inst, {
     reducerEnhancer: createEnhancer(options.reducerEnhancer),
     sentinels: createEnhancer(options.sentinels),
-    invaders: createEnhancer(options.invaders),
+    spies: createEnhancer(options.spies),
     guards: createEnhancer(options.guards),
   });
 }
--- packages/shared/interfaces/dx-pipe.interface.ts
+++
@@ -1,5 +0,0 @@
-import { Action } from 'redux';
-
-export interface BasePipeInterface {
-  pipe(action: Action): Generator<unknown, boolean | void>;
-}
