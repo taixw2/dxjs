1bd99dbc7d0e9b3d17ab4487fb85598e398f0ef5
mro <254225961@qq.com>
2020-05-06 17:42:01 +0800

    fix: 导出 effect 增强器

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,5 +2,8 @@ import { DxFactory, DxFactoryInterface } from './dx';
 import { connect } from 'react-redux';
 
 export { DxModel } from './dx-model/model';
+export { Disguiser } from './dx-model/disguiser';
+export { Guard } from './dx-model/guard';
+export { Sentinel } from './dx-model/sentinel';
 export { DxFactory, connect };
 export const Dx: DxFactoryInterface = DxFactory();
